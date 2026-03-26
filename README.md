144ba2a1e079bee68fd939e6bfb9bcf61dc0d4bc
Jinjie Liu <jjliu@baai.ac.cn>
2026-03-26 17:54:44 +0800

    setup the skeleton
    Signed-off-by: Jinjie Liu <jjliu@baai.ac.cn>

---
+++ README.md
@@ -0,0 +1,8 @@
+# LibTriton
+
+## Instruction
+
+### Debug Building
+```bash
+CC=clang CXX=clang++ uv pip install -ve . --no-build-isolation -Cbuild-dir=build
+```
---
+++ python/libtriton/mlir.py
@@ -0,0 +1,19 @@
+from libtriton._C.python_packages.mlir_core.mlir import (
+    dialects,
+    extras,
+    runtime,
+    execution_engine,
+    ir,
+    passmanager,
+    rewrite,
+)
+
+__all__ = [
+    "dialects",
+    "extras",
+    "runtime",
+    "execution_engine",
+    "ir",
+    "passmanager",
+    "rewrite",
+]

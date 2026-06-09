6eaf7caaf93c3bfeda883433651b19f170a63c6d
Jinjie Liu <sgjzfzzf@gmail.com>
2026-06-10 02:25:20 +0800

    fix: add TorchMLIRTorchUtils linkage, disable StableHLO/TOSA, and fix runtime install
    Signed-off-by: Jinjie Liu <sgjzfzzf@gmail.com>

--- tools/format.sh
+++ tools/format.sh
@@ -2,3 +2,4 @@
 
 find examples libtriton-core python test -name "*.py" | xargs ruff format
 find libtriton-core -name "*.c" -o -name "*.cc" -o -name "*.h" -o -name "*.hpp" -o -name "*.td" | xargs clang-format -i
+(find libtriton-core -name CMakeLists.txt && echo CMakeLists.txt) | xargs cmake-format -i

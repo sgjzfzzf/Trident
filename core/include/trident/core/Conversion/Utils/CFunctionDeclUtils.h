6bf039330bb02c2e77499c88ae70e1770344b128
Jinjie Liu <sgjzfzzf@gmail.com>
2026-09-06 22:41:49 +0800

    refactor(core): deduplicate C function type traits
    Signed-off-by: Jinjie Liu <sgjzfzzf@gmail.com>

--- core/include/trident/core/Conversion/Utils/CFunctionDeclUtils.h
+++ core/include/trident/core/Conversion/Utils/CFunctionDeclUtils.h
@@ -27,39 +27,82 @@ getOrCreateCAPIImpl(mlir::ModuleOp moduleOp, llvm::StringRef symbol,
 
 template <typename T> inline constexpr bool kUnsupportedCType = false;
 
-template <typename CType> struct CTypeToLLVM {
-  static mlir::Type get(mlir::MLIRContext *context) {
-    using BaseType = std::remove_cv_t<std::remove_reference_t<CType>>;
+template <typename CType, typename Enable = void> struct CTypeToLLVMImpl {
+  static mlir::Type get(mlir::MLIRContext * /*context*/) {
+    static_assert(kUnsupportedCType<CType>,
+                  "Unsupported C API type in CTypeToLLVM");
+  }
+};
 
-    if constexpr (std::is_same_v<BaseType, DLDataType>) {
-      return mlir::LLVM::LLVMStructType::getLiteral(
-          context,
-          {mlir::IntegerType::get(context, 8),
-           mlir::IntegerType::get(context, 8),
-           mlir::IntegerType::get(context, 16)},
-          /*isPacked=*/true);
-    } else if constexpr (std::is_same_v<BaseType, DLDevice>) {
-      return mlir::LLVM::LLVMStructType::getLiteral(
-          context,
-          {mlir::IntegerType::get(context, 32),
-           mlir::IntegerType::get(context, 32)},
-          /*isPacked=*/true);
-    } else if constexpr (std::is_integral_v<BaseType> ||
-                         std::is_enum_v<BaseType>) {
-      return mlir::IntegerType::get(context, sizeof(BaseType) * 8);
-    } else if constexpr (std::is_same_v<BaseType, float>) {
-      return mlir::Float32Type::get(context);
-    } else if constexpr (std::is_same_v<BaseType, double>) {
-      return mlir::Float64Type::get(context);
-    } else if constexpr (std::is_floating_point_v<BaseType>) {
-      static_assert(kUnsupportedCType<BaseType>,
-                    "Unsupported floating-point C type in CTypeToLLVM");
-    } else if constexpr (std::is_pointer_v<BaseType>) {
-      return mlir::LLVM::LLVMPointerType::get(context);
-    } else {
-      static_assert(kUnsupportedCType<BaseType>,
-                    "Unsupported C API type in CTypeToLLVM");
-    }
+template <> struct CTypeToLLVMImpl<DLDataType> {
+  static mlir::Type get(mlir::MLIRContext *context) {
+    return mlir::LLVM::LLVMStructType::getLiteral(
+        context,
+        {mlir::IntegerType::get(context, 8), mlir::IntegerType::get(context, 8),
+         mlir::IntegerType::get(context, 16)},
+        /*isPacked=*/true);
+  }
+};
+
+template <> struct CTypeToLLVMImpl<DLDevice> {
+  static mlir::Type get(mlir::MLIRContext *context) {
+    return mlir::LLVM::LLVMStructType::getLiteral(
+        context,
+        {mlir::IntegerType::get(context, 32),
+         mlir::IntegerType::get(context, 32)},
+        /*isPacked=*/true);
+  }
+};
+
+template <typename CType>
+struct CTypeToLLVMImpl<CType, std::enable_if_t<std::is_integral_v<CType> ||
+                                               std::is_enum_v<CType>>> {
+  static mlir::Type get(mlir::MLIRContext *context) {
+    return mlir::IntegerType::get(context, sizeof(CType) * 8);
+  }
+};
+
+template <typename CType>
+struct CTypeToLLVMImpl<CType, std::enable_if_t<std::is_same_v<CType, float> ||
+                                               std::is_same_v<CType, double>>> {
+  static mlir::Type get(mlir::MLIRContext *context) {
+    using FloatType = std::conditional_t<std::is_same_v<CType, float>,
+                                         mlir::Float32Type, mlir::Float64Type>;
+    return FloatType::get(context);
+  }
+};
+
+template <typename CType>
+struct CTypeToLLVMImpl<CType,
+                       std::enable_if_t<std::is_floating_point_v<CType> &&
+                                        !std::is_same_v<CType, float> &&
+                                        !std::is_same_v<CType, double>>> {
+  static mlir::Type get(mlir::MLIRContext *) {
+    static_assert(kUnsupportedCType<CType>,
+                  "Unsupported floating-point C type in CTypeToLLVM");
+  }
+};
+
+template <typename CType>
+struct CTypeToLLVMImpl<CType, std::enable_if_t<std::is_pointer_v<CType>>> {
+  static mlir::Type get(mlir::MLIRContext *context) {
+    return mlir::LLVM::LLVMPointerType::get(context);
+  }
+};
+
+template <typename CType>
+struct CTypeToLLVM
+    : CTypeToLLVMImpl<std::remove_cv_t<std::remove_reference_t<CType>>> {};
+
+template <typename Ret> struct CFunctionReturnType {
+  static mlir::Type get(mlir::MLIRContext *context) {
+    return CTypeToLLVM<Ret>::get(context);
+  }
+};
+
+template <> struct CFunctionReturnType<void> {
+  static mlir::Type get(mlir::MLIRContext *context) {
+    return mlir::LLVM::LLVMVoidType::get(context);
   }
 };
 
@@ -69,12 +112,7 @@ template <typename Ret, typename... Args>
 struct CFunctionSignature<Ret(Args...)> {
   using RetType = Ret;
   static mlir::LLVM::LLVMFunctionType getLLVMType(mlir::MLIRContext *context) {
-    mlir::Type retTy = // NOLINT(misc-const-correctness)
-        mlir::LLVM::LLVMVoidType::get(context);
-    if constexpr (!std::is_void_v<Ret>) {
-      retTy = CTypeToLLVM<Ret>::get(context);
-    }
-
+    const mlir::Type retTy = CFunctionReturnType<Ret>::get(context);
     const llvm::SmallVector<mlir::Type> argTypes = {
         CTypeToLLVM<Args>::get(context)...};
     return mlir::LLVM::LLVMFunctionType::get(retTy, argTypes);
@@ -86,13 +124,15 @@ template <typename FunctionType> struct CFunctionTraits {
                 "CFunctionTraits expects a function pointer type");
 };
 
-template <typename Ret, typename... Args>
-struct CFunctionTraits<Ret (*)(Args...)> {
+template <typename Ret, typename... Args> struct CFunctionTraitsImpl {
   using Signature = CFunctionSignature<Ret(Args...)>;
   static mlir::LLVM::LLVMFunctionType getLLVMType(mlir::MLIRContext *context) {
     return Signature::getLLVMType(context);
   }
 };
+
+template <typename Ret, typename... Args>
+struct CFunctionTraits<Ret (*)(Args...)> : CFunctionTraitsImpl<Ret, Args...> {};
 
 template <typename FunctionType>
 mlir::FailureOr<mlir::LLVM::LLVMFuncOp>
@@ -109,12 +149,8 @@ getOrCreateCAPI(mlir::ModuleOp moduleOp, llvm::StringRef symbol,
                 mlir::LLVM::LLVMFunctionType expectedType);
 
 template <typename Ret, typename... Args>
-struct CFunctionTraits<Ret (*)(Args...) noexcept> {
-  using Signature = CFunctionSignature<Ret(Args...)>;
-  static mlir::LLVM::LLVMFunctionType getLLVMType(mlir::MLIRContext *context) {
-    return Signature::getLLVMType(context);
-  }
-};
+struct CFunctionTraits<Ret (*)(Args...) noexcept>
+    : CFunctionTraitsImpl<Ret, Args...> {};
 
 } // namespace trident::conversion::utils
 
